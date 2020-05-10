ad9afea136883cbef060d93f674ece5ad84d4b28
Zmey <amenshchikov@gmail.com>
2020-05-10 20:35:25 +0300

    ProductPage refactored

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,14 +8,22 @@ class ProductPage(BasePage):
         button.click()
 
     def should_be_success_messages(self):
-        title = self.browser.find_element(*ProductPageLocators.ITEM_TITLE).text.strip()
-        price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text.strip()
+        self.should_be_messages()
+        self.should_be_title_in_messages()
+        self.should_be_price_in_messages()
 
+    def should_be_messages(self):
         assert self.is_element_present(*ProductPageLocators.MESSAGES), 'Success messages is not present'
+
+    def should_be_title_in_messages(self):
+        title = self.browser.find_element(*ProductPageLocators.ITEM_TITLE).text.strip()
 
         alert_title_locator = ProductPageLocators.ALERT_TITLE
         alert_title_locator[1] += f'[contains(text(), "{title}")]'
         assert self.is_element_present(*ProductPageLocators.ALERT_TITLE), 'Messages does not contain item title'
+
+    def should_be_price_in_messages(self):
+        price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text.strip()
 
         alert_price_locator = ProductPageLocators.ALERT_PRICE
         alert_price_locator[1] += f'[contains(text(), "{price}")]'
