54ed0a0f40cb2bd3d3f10cbee82a02a2f07d1edc
Zmey <amenshchikov@gmail.com>
2020-05-10 20:51:03 +0300

    ProductPage parametrize with xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,13 @@
+import pytest
+
 from pages.product_page import ProductPage
 
+urls = [f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{n}' for n in range(10)]
+urls[7] = pytest.param(urls[7], marks=pytest.mark.xfail)
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
