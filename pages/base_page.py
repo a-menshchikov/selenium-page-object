fe0fe87d161be6f36f63608c0ff2f3930394059e
Zmey <amenshchikov@gmail.com>
2020-05-11 00:33:53 +0300

    login tests moved up from mainpage to basepage + login tests on product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,8 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from pages.locators import BasePageLocators
+
 
 class BasePage:
     def __init__(self, browser, url):
@@ -60,3 +62,11 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print('No second alert presented')
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), \
+            'Login link is not presented'
