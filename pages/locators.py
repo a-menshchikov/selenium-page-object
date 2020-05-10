e21540f411532b025d522db1c6688008a6bcabd0
Zmey <amenshchikov@gmail.com>
2020-05-10 22:10:42 +0300

    explicit wait + additional tests

--- pages/locators.py
+++ pages/locators.py
@@ -14,7 +14,7 @@ class ProductPageLocators:
     ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
     ITEM_TITLE = (By.CSS_SELECTOR, 'h1')
     ITEM_PRICE = (By.CSS_SELECTOR, 'h1 + *')
-    MESSAGES = (By.CSS_SELECTOR, '#messages')
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages .alert-success')
     ALERT_TITLE = [
         By.XPATH,
         '//div[@id="messages"]/div[contains(@class, "alert-success")]/div[contains(@class, "alertinner")]//strong'
--- test_product_page.py
+++ test_product_page.py
@@ -10,6 +10,30 @@ urls[7] = pytest.param(urls[7], marks=pytest.mark.xfail)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
+    page.should_not_be_success_message()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
+    page.should_be_disappeared_success_message()
     page.should_be_success_messages()
+
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    page.open()
+    page.add_to_cart()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+    page.open()
+    page.add_to_cart()
+    page.should_be_disappeared_success_message()
